d9f5b056bd0fff4f722745a85c32fd6919aaf244
hermn <germuha@gmail.com>
2020-12-12 18:59:38 +0300

    Signed-off-by: hermn <germuha@gmail.com>

--- index.js
+++ index.js
@@ -56,8 +56,9 @@ vk.updates.on('message_new', async (context, next) => {
           throw error
         }
         if(results[0]){
-          connection.query(`UPDATE usres_vk SET vk_id = '${context.peerId}' WHERE 'usres_vk'.'id_p' LIKE ${id}`, (error, results, fields) => {
-            console.log(`UPDATE usres_vk SET vk_id = '${context.peerId}' WHERE 'usres_vk'.'id_p' LIKE ${id}`)
+          let q = 'UPDATE `usres_vk` SET `vk_id` = '+context.peerId+' WHERE `id_p` = ' + id;
+          connection.query(q, (error, results, fields) => {
+            console.log(q)
             console.log(results)
           if(error){
             context.send("Ошибка добавления id в базу, возможно, вы ввели его не правильно или ни одной записи с этим id нет в базе! Прочитайте инструкцию и попробуйте снова")
