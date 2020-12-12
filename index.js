493651ac554a324b144f62c8ed46b8ca009c4c27
hermn <germuha@gmail.com>
2020-12-12 17:59:24 +0300

    vk: last fix..

--- index.js
+++ index.js
@@ -26,8 +26,55 @@ connection.connect(err => {
   	console.log("Подключено успешно")
   }
 })
+//VK START
+const { VK } = require('vk-io');
+const { HearManager } = require('@vk-io/hear');
 
-const start = child_process.fork("./VK/index.js")
+
+const vk = new VK({
+  token: process.env.TOKEN
+});
+
+const hearManager = new HearManager();
+
+vk.updates.on('message_new', hearManager.middleware);
+
+hearManager.hear('/start', async (context) => {
+  await context.send(`
+    Вы разрешили отправлять сообщения
+    Введите команду /myid id, где id, ваш ид, чтобы добавить id аккаунта ПВ
+  `);
+});
+
+vk.updates.on('message_new', async (context, next) => {
+  if(context.text.startsWith('/myid')){
+    var id = context.text.split(" ")[1];
+    context.send("Попытка добавить в базу данных...")
+    var vk_id = {
+        vk_id: context.peerId
+      }
+      connection.query(`SELECT * FROM usres_vk WHERE id_p LIKE '${Number(id)}'`, (error, results, fields) => {
+        if (error) {
+          console.error('An error occurred while executing the query')
+          throw error
+        }
+        console.log("#1", results)
+        if(results[0]){
+          connection.query('INSERT INTO usres_vk SET ?', vk_id, (error, results, fields) => {
+          if(error){
+            console.error('An error occurred while executing the query')
+              throw error
+            }
+          })
+        }else context.send("Ошибка добавления id в базу, возможно, вы ввели его не правильно или ни одной записи с этим id нет в базе! Прочитайте инструкцию и попробуйте снова")
+    })
+  }
+})
+
+
+
+vk.updates.start().catch(console.error);
+//VK END
 
 
 function AddToLowDB(body, res){
